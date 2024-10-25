57be4bd022967025c6f243356437e4cc7e4927a5
mirco.cennamo <mirco.cennamo@avanade.com>
2024-10-25 17:02:04 +0200

    k8s pong service

---
+++ src/main/java/com/example/demo/PongApplication.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class PongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PongApplication.class, args);
+	}
+
+
+
+
+}
